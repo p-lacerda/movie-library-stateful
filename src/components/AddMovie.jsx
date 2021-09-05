00ca4fb9539a5850b468bb4d83df501fd011ef01
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 14:00:33 -0300

    Requisito 12 e 13 concluidos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -45,6 +45,14 @@ class AddMovie extends React.Component {
           Avaliação
           <input type="number" name="rating" data-testid="rating-input" value={ rating } onChange={ this.handleChange } />
         </label>
+        <label htmlFor="" data-testid="genre-input-label">
+          Gênero
+          <select name="genre" id="" value={ genre } onChange={ this.handleChange } data-testid="genre-input">
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
