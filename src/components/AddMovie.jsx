4959ff5ef63c05692b9956805332c234cf32d984
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 13:35:53 -0300

    Requisito 10 concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,17 +17,25 @@ class AddMovie extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
     const { onClick } = this.props;
-    const { title } = this.state;
+    const { subtitle, title, imagePath } = this.state;
     return (
       <form>
         <label htmlFor="" data-testid="title-input-label">
           Título
           <input type="text" name="title" data-testid="title-input" value={ title } onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="" data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" name="subtitle" data-testid="subtitle-input" value={ subtitle } onChange={ this.handleChange } />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input type="text" name="imagePath" data-testid="image-input" value={ imagePath } onChange={ this.handleChange } />
         </label>
       </form>
     );
