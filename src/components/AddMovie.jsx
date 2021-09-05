930b5c7de5c4cbe00fbe6be05a75882444db7bd0
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 13:29:17 -0300

    requisito 8 concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,13 +12,23 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  };
 
   render() {
     const { onClick } = this.props;
+    const { title } = this.state;
     return (
       <form>
-        <legend>Form</legend>
+        <label htmlFor="" data-testid="title-input-label">
+          Título
+          <input type="text" name="title" data-testid="title-input" value={ title } onChange={ this.handleChange } />
+        </label>
       </form>
     );
   }
