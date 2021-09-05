5169b7c3bed158907333425b0e27dce3592a1708
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 05:40:38 -0300

    Requisito 6 concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
+    const { onClick } = this.props;
     return (
       <>
       </>
