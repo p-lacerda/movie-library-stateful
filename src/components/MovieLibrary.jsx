9f8bee275ad350c3350611543c127b473ef968f7
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 14:11:06 -0300

    Requisitos 15 e 16 concluidos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,16 +5,24 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
   }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={ movies } />
         <AddMovie />
       </div>
     );
